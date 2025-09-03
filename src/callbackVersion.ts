d118b881a948050eaf5f3dc13bceec6b2a6d1f8f
Your Name <you@example.com>
2025-09-03 19:11:06 +0200

    second phase

--- src/callbackVersion.ts
+++ src/callbackVersion.ts
@@ -2,3 +2,19 @@ import * as https from 'https'
 
 const WEATHER_URL = 'httpps://api.open.meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true';
 const NEWS_URL = 'https://dummyjson.com/posts';
+
+function fetchData(url: string, callback: (error: Error | null, data?: any) => void) {
+    https.get(url, res => {
+        let data = '';
+        res.on('data', chunk => (data += chunk));
+        res.on('end', () => {
+            try {
+                callback(null, JSON.parse(data));
+            } catch (e) {
+                callback(new Error('Failed to parse JSON'));
+            }
+        });
+    }).on('error', err => callback(err));
+
+
+}
