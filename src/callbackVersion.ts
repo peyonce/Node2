9f07e5980032b00e33e24ad4e387341a7e43b905
Your Name <you@example.com>
2025-09-03 18:47:12 +0200

    weather

---
+++ src/callbackVersion.ts
@@ -0,0 +1,4 @@
+import * as https from 'https'
+
+const WEATHER_URL = 'httpps://api.open.meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true';
+const NEWS_URL = 'https://dummyjson.com/posts';
