588272afaaed80ac7bd31b1054c392699aacbe35
Your Name <you@example.com>
2025-09-03 20:13:08 +0200

    weather

---
+++ src/promiseVersion.ts
@@ -0,0 +1,5 @@
+import https from 'https';
+
+const WEATHER_URL = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true';
+const NEWS_URL = 'https://dummyjson.com/posts';
+
