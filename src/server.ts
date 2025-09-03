58d8a8303b299497ccc466d14ff80449b2a795fb
Your Name <you@example.com>
2025-09-03 22:23:01 +0200

    get method

--- src/server.ts
+++ src/server.ts
@@ -9,3 +9,19 @@ function sendJSON(res: ServerResponse, data: any, statusCode = 200) {
     res.writeHead(statusCode, { ' Content-Type': 'application/json; charset=utf-8' });
     res.end(JSON.stringify(data, null, 2));
 }
+
+function sendText(res: ServerResponse, text: string, statusCode = 200) {
+    res.writeHead(statusCode, { ' Content-Type': 'text/plain; charset=utf-8' });
+    res.end(text);
+}
+
+const server = createServer((req: IncomingMessage, res: ServerResponse) => {
+    if (req.method === 'GET') {
+        switch (req.url) {
+            case '/':
+                sendText(res, 'Welcome to Async Weather  & News Dashboard!');
+                break;
+        }
+    }
+}
+)
