57fcb146b11fbb7d721da8c07f811fd6c2975ef6
Your Name <you@example.com>
2025-09-04 00:09:35 +0200

    fixed error

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,15 @@ import { getWeatherAndNews as getWeatherAndNewsCalls } from './callbackVersion';
 import { getWeatherAndNews as getWeatherAndNewsPromise, getAllSimultaneously, getFastest } from './promiseVersion';
 import { getWeatherAndNews as getWeatherAndNewsAsync } from './asyncAwaitVersion';
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000;
 
 function sendJSON(res: ServerResponse, data: any, statusCode = 200) {
-    res.writeHead(statusCode, { ' Content-Type': 'application/json; charset=utf-8' });
+    res.writeHead(statusCode, { 'Content-Type': 'application/json; charset=utf-8' });
     res.end(JSON.stringify(data, null, 2));
 }
 
 function sendText(res: ServerResponse, text: string, statusCode = 200) {
-    res.writeHead(statusCode, { ' Content-Type': 'text/plain; charset=utf-8' });
+    res.writeHead(statusCode, { 'Content-Type': 'text/plain; charset=utf-8' });
     res.end(text);
 }
 
@@ -66,12 +66,13 @@ const server = createServer((req: IncomingMessage, res: ServerResponse) => {
                 sendText(res, 'Not Found', 404);
         }
     } else {
-        sendText(res, 'Method Not Allowed', 404);
+        sendText(res, 'Method Not Allowed', 405);
     }
 });
 
 server.listen(PORT, () => {
-    console.log('Server running at http://localhost:3000');
+    console.log(`Server running at http://localhost:${PORT}`);
+
 });
 
 
